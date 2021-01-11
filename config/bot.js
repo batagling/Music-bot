0d177c5385b2d6a59c8a7ed756e07f2dfe97a2a7
batagling <64485804+batagling@users.noreply.github.com>
2021-01-11 00:33:51 -0300

    Update bot.js

--- config/bot.js
+++ config/bot.js
@@ -8,9 +8,9 @@ module.exports = {
     },
 
     discord: {
-        token: 'TOKEN',
-        prefix: 'PREFIX',
-        activity: 'ACTIVITY',
+        token: 'Nzk4MDI2ODM3Mzk3NDcxMjMz.X_vCaA.WtcKAtMWr1m_VDrKmeUlCryptvs',
+        prefix: '.',
+        activity: 'MUSIC',
     },
 
     filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
